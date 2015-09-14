a6a953a9d7869e6a9e786b32a9995d2c14753c95
Chris Acuario <christine.acuario@dollarshaveclub.com>
2015-09-14 14:06:15 -0700

    Moved lookup to instance initializer

--- addon/initializers/route-layers.js
+++ addon/initializers/route-layers.js
@@ -1,41 +1,7 @@
 
 export function initialize (container, application) {
-
-  var routeLayersService = container.lookup('service:route-layers');
-
-  Ember.Route.reopen({
-
-    routeLayer: 'default',
-
-    afterModel: function (model, transition) {
-      this._super.apply(this, arguments);
-
-      // Leaf route only
-      var leafRouteName = transition.handlerInfos.get('lastObject.handler.routeName');
-      if (this.routeName !== leafRouteName) return;
-
-      transition.promise.then(() => {
-        if (transition.isAborted) return;
-        routeLayersService.push(transition);
-      });
-    },
-
-    _actions: { // @see https://github.com/emberjs/ember.js/issues/5394
-
-      exitRouteLayer: function () {
-        var exitPoint = routeLayersService.pop();
-        if (exitPoint) exitPoint.transition.retry();
-        else this.send('exitRouteLayerFallback');
-      },
-
-      exitRouteLayerFallback: function () {
-        this.transitionTo('index');
-      }
-
-    }
-
-  });
-
+  var RouteLayer = Ember.Object.extend();
+  application.register('service:route-layers', RouteLayer);
 }
 
 export default {
