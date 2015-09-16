e4e328556cf8ddf75556bcd6c23f79a04dfa851d
Patrick Fisher <patrick@pwfisher.com>
2015-09-15 22:37:50 -0700

    Kill instance-initializer and revert to _actions for Ember 1.13 compatibiility

--- addon/initializers/route-layers.js
+++ addon/initializers/route-layers.js
@@ -1,7 +1,43 @@
 import RouteLayers from 'ember-route-layers/services/route-layers';
 
 export function initialize (container, application) {
+
   application.register('service:route-layers', RouteLayers);
+  application.inject('route', 'routeLayers', 'service:route-layers');
+
+  Ember.Route.reopen({
+
+    routeLayer: 'default',
+
+    afterModel: function (model, transition) {
+      this._super.apply(this, arguments);
+
+      // Leaf route only
+      var leafRouteName = transition.handlerInfos.get('lastObject.handler.routeName');
+      if (this.routeName !== leafRouteName) return;
+
+      transition.promise.then(() => {
+        if (transition.isAborted) return;
+        this.routeLayers.push(transition);
+      });
+    },
+
+    _actions: {
+
+      exitRouteLayer: function () {
+        var exitPoint = this.routeLayers.pop();
+        if (exitPoint) exitPoint.transition.retry();
+        else this.send('exitRouteLayerFallback');
+      },
+
+      exitRouteLayerFallback: function () {
+        this.transitionTo('index');
+      }
+
+    }
+
+  });
+
 }
 
 export default {
--- app/instance-initializers/route-layers.js
+++
@@ -1,1 +0,0 @@
-export { default, initialize } from 'ember-route-layers/instance-initializers/route-layers';
