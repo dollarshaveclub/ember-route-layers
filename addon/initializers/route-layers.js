afc845ce1ac38d4ff06d114921156539a8e97e4a
Arjan Singh <arjan.singh@dollarshaveclub.com>
2015-11-09 17:41:41 -0800

    Use actions instead of _actions

--- addon/initializers/route-layers.js
+++ addon/initializers/route-layers.js
@@ -22,7 +22,7 @@ export function initialize (container, application) {
       });
     },
 
-    _actions: {
+    actions: {
 
       exitRouteLayer: function () {
         var exitPoint = this.routeLayers.pop();
