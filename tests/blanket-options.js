9b362f2782ac656a86bba7040fe17999a9b980d6
Arjan Singh <arjan.singh@dollarshaveclub.com>
2016-03-11 12:55:09 -0800

    Code Coverage

---
+++ tests/blanket-options.js
@@ -0,0 +1,22 @@
+/* globals blanket, module */
+
+var options = {
+  modulePrefix: 'ember-route-layers',
+  filter: '//.*ember-route-layers/.*/',
+  antifilter: '//.*(tests|template).*/',
+  loaderExclusions: [],
+  enableCoverage: true,
+  cliOptions: {
+   lcovOptions: {
+     outputFile: 'lcov.dat',
+     renamer: function (moduleName) { return moduleName.replace(/^ember-uni-form/, 'addon') + '.js'; }
+   },
+   reporters: ['lcov'],
+   autostart: true
+  }
+};
+if (typeof exports === 'undefined') {
+  blanket.options(options);
+} else {
+  module.exports = options;
+}
